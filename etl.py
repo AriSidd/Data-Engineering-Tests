27bd90498dedfa297eeb9f4f9830fa0a3c236b44
Kinza Malik <kiinza.malik@gmail.com>
2023-04-03 11:09:06 +0500

    running ETL job

---
+++ etl.py
@@ -0,0 +1,95 @@
+import glob                         # selecting files 
+import pandas as pd                 #  CSV files
+import xml.etree.ElementTree as ET  #  XML files.
+from datetime import datetime
+
+
+
+# CSV Extract Function
+def extract_from_csv():
+    dataframe = pd.read_csv('./Pokemon_full.csv')
+    return dataframe
+
+print(extract_from_csv())
+
+
+
+
+#JSON Extract Function
+def extract_from_json():
+    dataframe = pd.read_json('./intents.json',lines=True)
+    return dataframe
+print(extract_from_json())
+
+
+
+# XML Extraction Function
+def extract_from_xml(file_to_process):
+    dataframe = pd.DataFrame(columns=["name", "height", "weight"])
+    tree = ET.parse('')
+    root = tree.getroot()
+    for person in root:
+        name = person.find("name").text
+        height = float(person.find("height").text)
+        weight = float(person.find("weight").text)
+        dataframe = dataframe.append({"name":name, "height":height, "weight":weight}, ignore_index=True)
+    return dataframe
+
+
+
+# Extraction function 
+
+def extract():
+    extracted_data = pd.DataFrame(columns=['name','height','weight']) # create an empty data frame to hold extracted data
+    
+    #process all csv files
+    for csvfile in glob.glob("*.csv"):
+        extracted_data = extracted_data.append(extract_from_csv(csvfile), ignore_index=True)
+        
+    #process all json files
+    for jsonfile in glob.glob("*.json"):
+        extracted_data = extracted_data.append(extract_from_json(jsonfile), ignore_index=True)
+    
+    #process all xml files
+    for xmlfile in glob.glob("*.xml"):
+        extracted_data = extracted_data.append(extract_from_xml(xmlfile), ignore_index=True)
+        
+    return extracted_data
+
+
+
+# Transform
+
+def transform(data):
+        #Convert height which is in inches to millimeter
+        #Convert the datatype of the column into float
+        #data.height = data.height.astype(float)
+        #Convert inches to meters and round off to two decimals(one inch is 0.0254 meters)
+        data['height'] = round(data.height * 0.0254,2)
+        
+        #Convert weight which is in pounds to kilograms
+        #Convert the datatype of the column into float
+        #data.weight = data.weight.astype(float)
+        #Convert pounds to kilograms and round off to two decimals(one pound is 0.45359237 kilograms)
+        data['weight'] = round(data.weight * 0.45359237,2)
+        return data
+
+
+# Laoding
+
+def load(targetfile,data_to_load):
+    data_to_load.to_csv(targetfile)  
+
+
+
+# Logging
+def log(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second
+    now = datetime.now() # get current timestamp
+    timestamp = now.strftime(timestamp_format)
+    with open("logfile.txt","a") as f:
+        f.write(timestamp + ',' + message + '\n')
+
+
+
+ ## Running ETL Process
---
+++ webscraping.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup
+import html5lib
+import requests
+import pandas as pd
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://web.archive.org/web/20200318083015/https://en.wikipedia.org/wiki/List_of_largest_banks"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+
+table = soup.find("table", {"class": "wikitable sortable"})
+rows = table.findAll("tr")[1:]
+
+data = []
+
+for row in rows:
+    cols = row.findAll("td")
+    data.append({
+        "name": cols[1].text.strip(),
+        "market_cap": cols[2].text.strip()
+    })
+
+df = pd.DataFrame(data)
+df.columns = ['Bank Name', 'Market Cap (US$ Billion)']
+print(df.head())
+
+
+
+
+
+
+
+
+
+
+# url = "https://web.archive.org/web/20200318083015/https://en.wikipedia.org/wiki/List_of_largest_banks"
+# response = requests.get(url)
+# soup = BeautifulSoup(response.content, "html.parser")
+
+# table = soup.find("table", {"class": "wikitable sortable"})
+# rows = table.findAll("tr")[1:]
+
+# data = []
+
+# for row in rows:
+#     cols = row.findAll("td")
+#     data.append({
+#         "rank": cols[0].text.strip(),
+#         "name": cols[1].text.strip(),
+#         "market_cap": cols[2].text.strip(),
+#         "country": cols[3].text.strip()
+#     })
+
+# with open("largest_banks.json", "w") as f:
+#     json.dump(data, f)
